d0ff757411bf17df566aefaf9daef5e61f53e0c4
CourtHive <charles@courthive.com>
2021-01-11 21:05:02 +0100

    embellish

--- example/src/Configurator.jsx
+++ example/src/Configurator.jsx
@@ -18,13 +18,31 @@ const Configurator = () => {
   return (
     <>
       <Paper className={classes.paper} elevation={2}>
-        <Typography variant="h5" component="h3">
+        <Typography variant="h5" component="h3" style={{ marginBottom: "1em" }}>
+          {"TODS MatchUp Format Code Generator"}
+          <a href="https://itftennis.atlassian.net/wiki/spaces/TODS/pages/1272840309/MatchUp+Format+Code"></a>
+        </Typography>
+        <Typography variant="h5" component="h3" style={{ color: "blue" }}>
           {matchUpFormatCode.stringify(matchUpFormatParsed)}
         </Typography>
         <MatchUpFormatForm
           matchUpFormatParsed={matchUpFormatParsed}
           onChange={handleOnChange}
         />
+      </Paper>
+      <Paper className={classes.paper} elevation={2}>
+        <Typography variant="h5" component="h3" style={{ marginBottom: "1em" }}>
+          {"TODS: Tennis Open Data Standards"}
+        </Typography>
+        The goal of the Tennis Open Data Standards (TODS) project is to create a
+        single data exchange framework that allows the transfer of any
+        tennis-related data between different information systems using
+        consistent, agreed formats and methods.
+        <p>
+          <a href="https://itftennis.atlassian.net/wiki/spaces/TODS/pages/1272840309/MatchUp+Format+Code">
+            Read more here
+          </a>
+        </p>
       </Paper>
     </>
   );
